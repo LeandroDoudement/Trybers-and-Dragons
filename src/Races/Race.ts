85a05477232ae9fce2eed983dc5720980519dd8f
LeandroDoudement <leandrodoudement@gmail.com>
2023-03-24 16:23:59 -0300

    Requisito 2 | Dwarf, Elf, Halfling, Orc Races

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,12 +1,8 @@
 abstract class Race {
-  private static _instances = 0;
-
   constructor(
     private readonly _name: string,
-    private readonly _dexterity: number
-  ) {
-    Race._instances += 1;
-  }
+    private readonly _dexterity: number,
+  ) {}
 
   get name(): string {
     return this._name;
