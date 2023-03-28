11e8afe8788571fad6bd6fa08dd53dbb76aba8e2
LeandroDoudement <leandrodoudement@gmail.com>
2023-03-27 21:12:27 -0300

    Requisito 9 | Monster class

---
+++ src/Monster.ts
@@ -0,0 +1,28 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  constructor(
+    protected _lifePoints: number = 85,
+    protected _strength: number = 63,
+  ) {}
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    this._lifePoints -= attackPoints;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
+
+export default Monster;
